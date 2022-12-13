ba533137f57215100cdc4b1bdd7eb38f6e4e99d6
Thevarasa Sangaran <93479754+TJSangaran@users.noreply.github.com>
2022-12-13 08:46:01 +0530

    Update PersistentTransectionDAO.java

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentTransectionDAO.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentTransectionDAO.java
@@ -1,1 +1,23 @@
 
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.Constants;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
+
+
+public class PersistentTransactionDAO implements TransactionDAO{
+    SQLiteDatabase dbManager;
+    java.io.File readfile = Constants.CONTEXT.getFilesDir();
+    public PersistentTransactionDAO()
+    {
+        dbManager = SQLiteDatabase.openOrCreateDatabase(file.getAbsolutePath() + "/tjsangaran.sqlite", null);
+        dbManager.execSQL("CREATE TABLE IF NOT EXISTS InfoTransection(accountNo VARCHAR(30),expenseType VARCHAR(20),amount NUMERIC(15,2), date_value Date);");
+    }
+}
